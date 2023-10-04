0b6d44d5b03f45e1d407165b859f813a5701b06f
crazyfrog-phongee <trongphong1708@gmail.com>
2023-10-04 09:37:46 +0700

    Update comments and exception

--- Linux-System-Programming/01-Shell-Script-Programming/calculator.sh
+++ Linux-System-Programming/01-Shell-Script-Programming/calculator.sh
@@ -7,14 +7,27 @@ then
 elif [ $# -eq 3 ]
 then
   case $2 in
-    +) sum=`expr "scale=2; $1 + $3" | bc`
-      echo "$1 + $3 = $sum" ;;
-    -) sub=`expr "scale=2; $1 - $3" | bc`
-      echo "$1 - $3 = $sub" ;;
-    x) mul=`expr "scale=2; $1 "*" $3" | bc`
-      echo "$1 x $3 = $mul" ;;
-    /) div=`expr "scale=2; $1 / $3" | bc`
-      echo "$1 / $3 = $div" ;;
+    +) 
+      sum=`expr "scale=2; $1 + $3" | bc`
+      echo "$1 + $3 = $sum" 
+      ;;
+    -) 
+      sub=`expr "scale=2; $1 - $3" | bc`
+      echo "$1 - $3 = $sub" 
+      ;;
+    x) 
+      mul=`expr "scale=2; $1 "*" $3" | bc`
+      echo "$1 x $3 = $mul" 
+      ;;
+    /)
+      if [ $3 -eq 0 ]; then
+        echo "Cannot divide by 0"
+        exit 1
+      else
+        div=`expr "scale=2; $1 / $3" | bc`
+        echo "$1 / $3 = $div" 
+      fi
+      ;;
   esac
 else
   echo "Error: Please pass 3 arguments."
--- Linux-System-Programming/01-Shell-Script-Programming/lonely_integers.sh
+++ Linux-System-Programming/01-Shell-Script-Programming/lonely_integers.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-#There are N integers in an array A. All but one integer occur in pairs. Your task is to find the number that occurs only once
+# There are N integers in an array A. All but one integer occur in pairs. Your task is to find the number that occurs only once
+
+# Solution: 
+# "arr=($(cat))" in bash will read data from input and assign it to an array named "arr".
+# The "cat" command is used to read data from stdin (standard input).
+# 1. `echo "${arr[@]}"`: Prints all elements in array `arr`.
+# 2. `tr ' ' '\n'`: Replaces spaces with newlines, converting from a list to a vertical list.
+# 3. `sort`: Sort elements in lexicographic order.
+# 4. `uniq -u`: Filter out only non-duplicate elements.
+# 5. `tr '\n' ' ``: Replace newlines with spaces, converting from a vertical list to a horizontal list.
+
 read
 arr=($(cat)) 
 echo "${arr[@]}" | tr ' ' '\n' |sort | uniq -u | tr '\n' ' '
